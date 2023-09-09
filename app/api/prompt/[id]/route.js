0e5e7490f3bc3677efcafc3ff40009d98ca6478a
Umar <muhammadshafqatali3@gmail.com>
2023-09-09 13:17:37 +0500

    curd operations on posts

--- app/api/prompt/[id]/route.js
+++ app/api/prompt/[id]/route.js
@@ -1,6 +1,7 @@
 import promptsModel from "@models/prompt";
 import { connectdb } from "@utils/dbConnection";
 
+// get route
 export const GET = async (request, { params }) => {
   try {
     await connectdb();
@@ -9,7 +10,7 @@ export const GET = async (request, { params }) => {
         status: 500,
       });
     }
-    const prompt = await promptsModel.findById(params.id).populate("creator");
+    const prompt = await promptsModel.findById(params.id);
     return new Response(JSON.stringify(prompt), { status: 200 });
   } catch (error) {
     return new Response(JSON.stringify("Failed to fetch post"), {
@@ -17,19 +18,49 @@ export const GET = async (request, { params }) => {
     });
   }
 };
+
+// update route
 export const PATCH = async (request, { params }) => {
   try {
     const { updatedPrompt, updatedTag } = await request.json();
     await connectdb();
     if (!params) {
-      return new Response(JSON.stringify("Failed to fetch the post"), {
+      return new Response(JSON.stringify("Failed to update the post"), {
         status: 500,
       });
     }
-    const prompt = await promptsModel.findById(params.id).populate("creator");
-    return new Response(JSON.stringify(prompt), { status: 200 });
+    const existingPrompt = await promptsModel.findById(params.id);
+    if (!existingPrompt) {
+      return new Response(JSON.stringify("Failed to update the post"), {
+        status: 500,
+      });
+    }
+    existingPrompt.prompt = updatedPrompt;
+    existingPrompt.tag = updatedTag;
+    await existingPrompt.save;
+    return new Response(JSON.stringify(existingPrompt), { status: 200 });
   } catch (error) {
-    return new Response(JSON.stringify("Failed to fetch post"), {
+    return new Response(JSON.stringify("Failed to update the post"), {
+      status: 500,
+    });
+  }
+};
+
+// delete route
+export const DELETE = async (request, { params }) => {
+  try {
+    await connectdb();
+    if (!params) {
+      return new Response(JSON.stringify("Failed to delete the post"), {
+        status: 500,
+      });
+    }
+    await promptsModel.findByIdAndDelete(params.id);
+    return new Response(JSON.stringify("prompt successfully deleted"), {
+      status: 200,
+    });
+  } catch (error) {
+    return new Response(JSON.stringify("Failed to delete the post"), {
       status: 500,
     });
   }
